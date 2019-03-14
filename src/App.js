9539904bcbbe518b72b01a1d285903d10ec4b4c7
Saad Ahmed <skahmed@ryerson.ca>
2019-03-13 22:38:26 -0400

    added new navbar and embedded existing components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
-import Navbar from './components/Navbar';
-import Login from './components/Login';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Home from './components/Pages/Home';
+import Login from './components/Pages/Login';
+import Signup from './components/Pages/Signup';
+import Dashboard from './components/Pages/Dashboard';
+import Nav from './components/Appbar';
+
 import './App.css';
 
 class App extends Component {
+
+
   render() {
     return (
-      <div className="App">
-      <Navbar />
-        <h1>Log in</h1>
-        <Login></Login>
-      </div>
+      <Nav>
+              <Router>
+
+<div className="App">
+
+  <div className="container">
+    <Route exact path="/" component={Login} />
+
+    <Route path="/dashboard" component={Dashboard}/>
+
+  </div>
+  
+</div>
+
+</Router>
+      </Nav>
+
     );
   }
 }
--- src/components/Pages/Login.js
+++ src/components/Pages/Login.js
@@ -3,50 +3,79 @@ import Paper from '@material-ui/core/Paper';
 import Avatar from '@material-ui/core/Avatar'
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 import { FormControl, InputLabel, FormControlLabel, Checkbox } from '@material-ui/core';
 import Input from '@material-ui/core/Input';
 
 const styles = {
     main: {
+        padding: 20,
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center'
     },
     paperStyle: {
         width:400,
-        margin: '30px',
+        margin: 30,
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
-        padding:'20px'
+        padding:'40px'
     }
 }
 
 class Login extends Component {
-  render() {
-      console.log(this.props);
+
+    state = {
+        email: '',
+        password: ''
+    }
+
+    handleChange = (e) => {
+        console.log(e.target.value);
+        this.setState({
+            [e.target.id]: e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(this.state);
+    }
+
+    render() {
+
     return (
       <div style={styles.main}>
+      <Typography component="h1" variant="h5">
+          Login
+      </Typography>
         <Paper style={styles.paperStyle}>
             <Avatar>
                 <LockOutlinedIcon />
             </Avatar>
-            <Typography component="h1" variant="h5">
-                    Sign In
-            </Typography>
-            <form>
+
+            <form onSubmit={this.handleSubmit}>
                 <FormControl>
                     <InputLabel htmlFor="email">Email Address</InputLabel>
-                    <Input id="email" name="email" autoComplete="email" autoFocus />
+                    <Input id="email" name="email" autoComplete="email" onChange={this.handleChange} autoFocus />
                 </FormControl><br></br>
                 <FormControl>
                     <InputLabel htmlFor="password">Password</InputLabel>
-                    <Input id="password" name="password" type="password" autoComplete="current-password"/>
+                    <Input id="password" name="password" type="password" onChange={this.handleChange} autoComplete="current-password"/>
                 </FormControl><br></br>
                 <FormControlLabel 
                     control={<Checkbox value="remember" color="primary"/>}
                     label="Remember Me"
                 />
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                >
+                Sign In
+                </Button>
             </form>
         </Paper>
       </div>
